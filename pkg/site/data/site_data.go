765e7af94c5ba7db834bbf53783f2552e1e2b675
An Le <ledaian41@gmail.com>
2025-02-20 21:26:34 +0700

    update log

--- pkg/site/data/site_data.go
+++ pkg/site/data/site_data.go
@@ -2,30 +2,30 @@ package site_data
 
 import (
 	"encoding/json"
-	"fmt"
-	site_model "go-auth-service/pkg/site/model"
+	"go-auth-service/pkg/site/model"
 	"io"
+	"log"
 	"os"
 )
 
 func SiteData() []site_model.Site {
 	file, err := os.Open("./pkg/site/data/siteData.json")
 	if err != nil {
-		fmt.Println("Error opening file:", err)
+		log.Fatalf("❌ Error opening file: %v", err)
 		return nil
 	}
 	defer file.Close()
 
 	bytes, err := io.ReadAll(file)
 	if err != nil {
-		fmt.Println("Error reading file:", err)
+		log.Fatalf("❌ Error reading file: %v", err)
 		return nil
 	}
 
 	var sites []site_model.Site
 	err = json.Unmarshal(bytes, &sites)
 	if err != nil {
-		fmt.Println("Error unmarshalling JSON:", err)
+		log.Fatalf("❌ Error unmarshalling JSON: %v", err)
 		return nil
 	}
 
