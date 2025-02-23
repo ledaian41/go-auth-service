c4c2f11b0ddc9d3e6ded37e10713f39868ad301a
An Le <ledaian41@gmail.com>
2025-02-23 19:31:40 +0700

    load nodeType schema, support api delete

--- pkg/site/data/site_data.go
+++ pkg/site/data/site_data.go
@@ -2,6 +2,7 @@ package site_data
 
 import (
 	"encoding/json"
+	"fmt"
 	"go-auth-service/pkg/site/model"
 	"io"
 	"log"
@@ -11,21 +12,21 @@ import (
 func SiteData() []site_model.Site {
 	file, err := os.Open("./pkg/site/data/siteData.json")
 	if err != nil {
-		log.Fatalf("❌ Error opening file: %v", err)
+		log.Println(fmt.Sprintf("❌ Error opening file: %v", err))
 		return nil
 	}
 	defer file.Close()
 
 	bytes, err := io.ReadAll(file)
 	if err != nil {
-		log.Fatalf("❌ Error reading file: %v", err)
+		log.Println(fmt.Sprintf("❌ Error reading file: %v", err))
 		return nil
 	}
 
 	var sites []site_model.Site
 	err = json.Unmarshal(bytes, &sites)
 	if err != nil {
-		log.Fatalf("❌ Error unmarshalling JSON: %v", err)
+		log.Println(fmt.Sprintf("❌ Error unmarshalling JSON: %v", err))
 		return nil
 	}
 
